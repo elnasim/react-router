14c396b8487b98b97b0ad8c6102bb039ac5f7e16
Serega KTyH <archlis21@gmail.com>
2018-03-22 11:47:12 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Main from "./components/Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
+          <Link to='/'>Home</Link>
+          <Link to='/roster'>roster</Link>
+          <Link to='/schedule'>schedule</Link>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main/>
       </div>
     );
   }
---
+++ src/components/FullRoster.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class FullRoster extends Component {
+  render() {
+    return (
+      <div>FullRoster</div>
+    );
+  }
+}
+
+export default FullRoster;
